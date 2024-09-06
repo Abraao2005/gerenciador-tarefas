46d57f217f7fba55b16a9bcd62c938b7b29285a2
abraao2005 <abraao20001@gmail.com>
2024-09-06 04:10:46 -0300

    feat: template padrão

---
+++ resources/views/tarefas/index.blade.php
@@ -0,0 +1,13 @@
+@extends("template.main")
+@section("conteudo")
+
+<div class="d-flex justify-content-center align-items-center vh-100">
+<div class="container">
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
+</div>
+</div>
+  
+@endsection
